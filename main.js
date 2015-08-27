9f119f1d350707b0ce12b1feb0bd69598702af65
chiizi <m33pfish@gmail.com>
2015-08-27 18:40:49 -0500

    Fix annoying size issue

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ var main;
   var canvas = document.getElementById("frame");
   var ctx = canvas.getContext("2d");
   
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
   canvas.x = canvas.y = 0;
   
   var player = {
