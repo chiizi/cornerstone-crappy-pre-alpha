126a7ab6bd12f2ddad45c84cc58c51d6f206ebcf
chiizi <m33pfish@gmail.com>
2015-08-31 15:14:28 -0500

    Move core code from main.js to core.js

--- core.js
+++ core.js
@@ -1,1 +1,36 @@
-
+var main;
+main = (function() {
+  window.mainScopeExec = function(name) {
+    return name(); // yay no more eval :3
+  };
+  
+  var canvas = document.getElementById("frame");
+  var ctx = canvas.getContext("2d");
+  
+  (window.onresize = function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  })();
+  
+  canvas.x = canvas.y = 0;
+  
+  var iteration = 0;
+  
+  var keysDown = {};
+  
+  addEventListener("keydown", function (e) {
+    keysDown[e.keyCode] = true;
+  }, false);
+  addEventListener("keyup", function (e) {
+    delete keysDown[e.keyCode];
+  }, false);
+  
+  return function() {
+    update();
+    
+    render();
+    
+    iteration++;
+    window.requestAnimationFrame(main);
+  };
+})();
